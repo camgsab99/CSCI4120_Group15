df46cd5c5d8c8eb359212323f2bd20266ee8ac69
Cameron Sabiston <camgsab99@live.com>
2022-09-12 14:03:04 -0400

    KNN Proto

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,65 +1,62 @@
-import pandas as pd
-import random
 import math
-import operator
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
 
 def loadDataset(filename, split):
     trainingSet = []
     testSet = []
-    df = pd.read_csv(url, header=None)
+    df = pd.read_csv(filename, header=None)
     array = df.to_numpy()
     random.shuffle(array)
-    training_len = int(len(array)*split)
+    training_len = int(len(array) * split)
     trainingSet = array[:training_len]
     testSet = array[training_len:]
     return trainingSet, testSet
 
-def euclideanDistance(instance1, instance2, length):
-    distance = 0
-    for x in range(length):
-        distance += pow((instance1[x] - instance2[x]), 2)
-    return math.sqrt(distance)
 
-def getNeighbors(trainingSet, testInstance, k):
-    distances = []
-    length = len(testInstance)-1
-    for x in range(len(trainingSet)):
-        dist = euclideanDistance(testInstance, trainingSet[x], length)
-        distances.append((trainingSet[x], dist))
-    distances.sort(key=operator.itemgetter(1))
-    neighbors = []
-    for x in range(k):
-        neighbors.append(distances[x][0])
-    return neighbors
+def getPredictionSkLearn(trainingSet, testSet, k, attributes):
+    model = KNeighborsClassifier(n_neighbors=k)
+    trainingData = np.array(trainingSet)
+    trainingX = trainingData[:, 0:attributes]
+    trainingY = trainingData[:, attributes]
+    model.fit(trainingX, trainingY)
 
-def getResponse(neighbors):
-    classVotes = {}
-    for x in range(len(neighbors)):
-        response = neighbors[x][-1]
-        if response in classVotes:
-            classVotes[response] += 1
-        else:
-            classVotes[response] = 1
-    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedVotes[0][0]
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for x in range(len(testSet)):
-        if testSet[x][-1] == predictions[x]:
-            correct += 1
-    return (correct/float(len(testSet))) * 100.0
-
-data1 = [2, 2, 2, 'a']
-data2 = [4, 4, 4, 'b']
-distance = euclideanDistance(data1, data2, 3)
-print(distance)
-
-trainingSet = []
-testSet = []
-url = 'https://raw.githubusercontent.com/ruiwu1990/CSCI_4120/master/KNN/iris.data'
-trainingSet, testSet = loadDataset(url, 0.66)
+    testData = np.array(testSet)
+    testX = testData[:, 0:attributes]
+    testY = testData[:, attributes]
+    predictions = model.predict(testX)
+    accuracy = metrics.accuracy_score(testY, predictions)
+    return predictions, accuracy
 
 
-print('Train: ' + repr(len(trainingSet)))
-print('Test: ' + repr(len(testSet)))
+def main():
+    url = 'https://raw.githubusercontent.com/ruiwu1990/CSCI_4120/master/KNN/iris.data'
+    split = 0.67
+    trainingSet, testSet = loadDataset(url, split)
+    accuracies = {}
+    for r in range(5):
+        for k in range(1, 21):
+            predictions, accuracy = getPredictionSkLearn(trainingSet, testSet, k, 4)
+            if k not in accuracies.keys():
+                accuracies[k] = []
+            accuracies[k].append(accuracy)
+    averageAccuracies = {}
+    for a in accuracies.keys():
+        averageAccuracies[a] = sum(accuracies[a]) / len(accuracies[a])
+
+    x = averageAccuracies.keys()
+    y = averageAccuracies.values()
+    plt.plot(x, y)
+    xint = range(min(x), math.ceil(max(x)) + 1)
+    plt.xticks(xint)
+    plt.title('K Neighbors vs Average Accuracy')
+    plt.xlabel('K Neighbors')
+    plt.ylabel('Average Accuracy')
+    plt.show()
+
+main()
